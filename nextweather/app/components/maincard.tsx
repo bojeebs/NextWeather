006cd3d24f7cf11bc941fe96ab3ed874e2ef53a2
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2024-03-29 15:34:02 -0500

    Api Call setup

--- nextweather/app/components/maincard.tsx
+++ nextweather/app/components/maincard.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import {Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle,} from "@/components/ui/card"
+import { useWeatherStore } from '@/lib/store'
 
+const interface
 
 const Maincard = () => {
+
+  const { setWeatherData } = useWeatherStore
+  const apiKey = process.env.WEATHER_API
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=38.781151&lon=-90.486931&appid=${apiKey}&units=metric`);
+      const data = await response.json();
+      setWeatherData(data); 
+      console.log(data)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
+
+
+
   return (
     <div>
       <Card className='main-card w-72 bg-custom-dark border-custom transform translate-x-16 translate-y-16 '>
